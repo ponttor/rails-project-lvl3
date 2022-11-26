81630c62d5fe96abd952baaa5b870be59501c8cc
Alexander Illarionov <alex@heimstudio.ru>
2022-11-26 19:45:46 +0100

    minor fixes

--- test/controllers/web/admin/bulletins_controller_test.rb
+++ test/controllers/web/admin/bulletins_controller_test.rb
@@ -1,48 +1,43 @@
 # frozen_string_literal: true
-
 require 'test_helper'
 
-module Web
-  module Admin
-    class BulletinsControllerTest < ActionDispatch::IntegrationTest
-      setup do
-        sign_in users(:admin)
-      end
+class Web::Admin::BulletinsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:admin)
+  end
 
-      test 'index' do
-        get admin_root_url
-        assert_response :success
-      end
+  test 'index' do
+    get admin_root_url
+    assert_response :success
+  end
 
-      test 'archive' do
-        @bulletin = bulletins(:published)
+  test 'archive' do
+    @bulletin = bulletins(:published)
 
-        patch archive_admin_bulletin_url(@bulletin)
+    patch archive_admin_bulletin_url(@bulletin)
 
-        assert_redirected_to admin_root_path
-        @bulletin.reload
-        assert { @bulletin.archived? }
-      end
+    assert_redirected_to admin_root_path
+    @bulletin.reload
+    assert { @bulletin.archived? }
+  end
 
-      # test 'publish' do
-      #   @bulletin = bulletins(:under_moderation)
+  test 'publish' do
+    @bulletin = bulletins(:under_moderation)
 
-      #   patch publish_admin_bulletin_url(@bulletin)
+    patch publish_admin_bulletin_url(@bulletin)
 
-      #   assert_redirected_to admin_root_path
-      #   @bulletin.reload
-      #   assert { @bulletin.published? }
-      # end
+    assert_redirected_to admin_root_path
+    @bulletin.reload
+    assert { @bulletin.published? }
+  end
 
-      test 'reject' do
-        @bulletin = bulletins(:published)
+  test 'reject' do
+    @bulletin = bulletins(:published)
 
-        patch reject_admin_bulletin_url(@bulletin)
+    patch reject_admin_bulletin_url(@bulletin)
 
-        assert_redirected_to admin_root_path
-        @bulletin.reload
-        assert { @bulletin.rejected? }
-      end
-    end
+    assert_redirected_to admin_root_path
+    @bulletin.reload
+    assert { @bulletin.rejected? }
   end
 end
